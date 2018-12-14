c97006613576bb04659036bcaee709e825ada79a
Grzegorz Adamowicz <grzegorz@adamowicz.cx>
2018-12-14 11:46:25 +0100

    Change target load balancer health check

--- 01_ec2_autoscaling/load-balancer.tf
+++ 01_ec2_autoscaling/load-balancer.tf
@@ -20,7 +20,7 @@ resource "aws_elb" "default-elb" {
     healthy_threshold   = 2
     unhealthy_threshold = 2
     timeout             = 3
-    target              = "HTTP:80/status"
+    target              = "HTTP:80/"
     interval            = 30
   }
 
