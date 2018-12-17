23f60bcb45ad8bbca7f9201d6a85c986a89eb18b
Grzegorz Adamowicz <grzegorz@adamowicz.cx>
2018-12-17 14:29:26 +0100

    Fix route associations

--- 01_ec2_autoscaling/main.tf
+++ 01_ec2_autoscaling/main.tf
@@ -176,18 +176,18 @@ resource "aws_route_table" "private_c" {
 }
 
 resource "aws_route_table_association" "private_a" {
-  subnet_id      = "${element(aws_subnet.private_a}"
-  route_table_id = "${element(aws_route_table.private_a.id}"
+  subnet_id      = "${aws_subnet.private_a.id}"
+  route_table_id = "${aws_route_table.private_a.id}"
 }
 
 resource "aws_route_table_association" "private_b" {
-  subnet_id      = "${element(aws_subnet.private_b}"
-  route_table_id = "${element(aws_route_table.private_b.id}"
+  subnet_id      = "${aws_subnet.private_b.id}"
+  route_table_id = "${aws_route_table.private_b.id}"
 }
 
 resource "aws_route_table_association" "private_c" {
-  subnet_id      = "${element(aws_subnet.private_c}"
-  route_table_id = "${element(aws_route_table.private_c.id}"
+  subnet_id      = "${aws_subnet.private_c.id}"
+  route_table_id = "${aws_route_table.private_c.id}"
 }
 
 resource "aws_route" "private_nat_gateway_a" {
